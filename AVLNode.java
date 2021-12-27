76e94e256835ea127a57d2dd8ad1775d139ac332
Zia Al Khair Mohammed <zia842@gmail.com>
2021-12-27 14:04:40 -0500

    Create AVLNode.java

---
+++ AVLNode.java
@@ -0,0 +1,24 @@
+package com.ds.algo;
+
+public class AVLNode {
+	
+	public int data;
+	
+	public int height;
+	
+	public AVLNode leftChild;
+	
+	public AVLNode rightChild;
+	
+	public AVLNode(int data, int height, AVLNode leftChild, AVLNode rightChild) {
+		super();
+		this.data = data;
+		this.height = height;
+		this.leftChild = leftChild;
+		this.rightChild = rightChild;
+	}
+
+
+	
+
+}
