d2146e250fe7d5944ec7c97a9d3201eb0fdba3b8
Zia Al Khair Mohammed <zia842@gmail.com>
2021-12-27 14:06:35 -0500

    Create BinaryTreeNode.java

---
+++ BinaryTreeNode.java
@@ -0,0 +1,16 @@
+package com.ds.algo;
+
+public class BinaryTreeNode 
+{
+
+	int data;
+	BinaryTreeNode left;
+	BinaryTreeNode right;
+	int depth;
+	int horizontalDistance;
+	
+	public BinaryTreeNode(int data) {
+		this.data = data;
+	}
+	
+}
